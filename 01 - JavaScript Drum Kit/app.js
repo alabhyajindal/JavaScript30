df2fff58b3c9e976b5f4dff69a6cf781843d46a1
Alabhya Jindal <alabhya10@gmail.com>
2022-02-14 21:43:14 +0530

    transitionend

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,34 +1,16 @@
-const clap = document.getElementById("a");
-const hihat = document.getElementById("s");
-const kick = document.getElementById("d");
-const openhat = document.getElementById("f");
-const boom = document.getElementById("g");
-const ride = document.getElementById("h");
-const snare = document.getElementById("j");
-const tom = document.getElementById("k");
-const tink = document.getElementById("l");
+"use strict";
 
 window.addEventListener("keydown", (e) => {
-  if (e.key == "a") {
-    clap.currentTime = 0;
-    clap.play();
-  } else if (e.key == "s") {
-    hihat.currentTime = 0;
+  // Playing the sound when the key is pressed
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (audio == null) return;
+  audio.play();
+  audio.currentTime = 0;
 
-    hihat.play();
-  } else if (e.key == "d") {
-    kick.play();
-  } else if (e.key == "f") {
-    openhat.play();
-  } else if (e.key == "g") {
-    boom.play();
-  } else if (e.key == "h") {
-    ride.play();
-  } else if (e.key == "j") {
-    snare.play();
-  } else if (e.key == "k") {
-    tom.play();
-  } else if (e.key == "l") {
-    tink.play();
-  }
+  // Changing the class on when the key is pressed
+  let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  key.classList.add("playing");
+  key.addEventListener("transitionend", () => {
+    key.classList.remove("playing");
+  });
 });
